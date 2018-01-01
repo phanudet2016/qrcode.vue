743cdc354f555aeeeeff9e3901b25237a510618d
phanudet2016 <5706021622141@fitm.kmutnb.ac.th>
2018-01-01 07:54:20 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <input type="text" v-model="value">
+    <qrcode-vue :value="value" :size="size" level="H"></qrcode-vue>
+  </div>
+</template>
+
+<script>
+import QrcodeVue from 'qrcode.vue'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      value: '',
+      size: 300
+    }
+  },
+  components: {
+    QrcodeVue
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
